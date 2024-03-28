40d90a4c3975d7f635b59eb1076795ec2401c4f1
Fizzled <Fizzled04@gmail.com>
2024-03-28 06:11:04 -0500

    Initial commit

---
+++ src/components/Contact.svelte
@@ -0,0 +1,54 @@
+<script>
+    import { Modals, closeModal, openModal, modals } from 'svelte-modals'
+    import Modal from './Modal.svelte'
+      
+    function handleOpen() {
+        openModal(Modal, { 
+            title: `Contact Me`, 
+        })
+    }
+</script>
+
+<button on:click={handleOpen}>
+    Contact Me
+</button>
+
+<Modals>
+    <div
+        slot="backdrop"
+        class="backdrop"
+        on:click={closeModal}
+    />
+</Modals>
+
+<style>
+    .backdrop {
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        right: 0;
+        left: 0;
+        background: black;
+        opacity: 50%;
+        transition: 0.5s;
+    }
+    button {
+        background-color: rgb(238, 198, 108);
+        border: none;
+        padding: 12px 20px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        margin: 4px 2px;
+        cursor: pointer;
+        border-radius: 1000px;
+        font-size: 15px;
+        color: #102542;
+        font-weight: bold;
+        transition: .25s;
+    }
+    button:hover {
+        background-color: rgb(202, 167, 86);
+        transition: 0.25s;
+    }
+</style>
---
+++ src/components/Modal.svelte
@@ -0,0 +1,221 @@
+<script>
+    import { closeModal, closeAllModals, openModal, modals } from 'svelte-modals'
+
+    function isValidEmail(email) {
+      // Regular expression for validating email addresses
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      return emailRegex.test(email);
+    }   
+
+    export let isOpen;
+    export let title;
+      let stackIndex = modals.length
+      let email = '';
+      let subject = '';
+      let message2 = '';
+
+    function validateEmail() {
+      if (!isValidEmail(email)) {
+          return false;
+      }
+      return true;
+    }
+  
+      $: isFormFilled = email && subject && message2;
+    
+    async function handleSubmit() {
+      if (validateEmail()) {
+        if (isFormFilled) {
+        const requestBody = {
+          email: email,
+          subject: subject,
+          message2: message2
+        };
+
+        fetch('http://localhost:3000/api/send-email', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(requestBody)
+          })
+          .then(response => {
+            if (response.ok) {
+              console.log('Email sent successfully');
+              closeModal();
+            } else {
+              console.error('Failed to send email');
+            }
+          })
+          .catch(error => {
+            console.error('Error sending email:', error);
+          })
+        }
+        }
+    }
+</script>
+
+{#if isOpen}
+  <div class="modal">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h2>{title}</h2>
+        <div id="actions">
+          <button id="close" on:click={closeModal}>
+            <img src="x-symbol-svgrepo-com.svg"/>
+          </button>	
+        </div>				
+      </div>
+      <form on:submit|preventDefault={handleSubmit}>
+        <div id="modal-body">
+          <input id="inputs" type="text" bind:value={email} name="email" placeholder="Email">
+          <input id="inputs2" type="text" bind:value={subject} name="subject" placeholder="Subject"> 
+        </div>
+        <div>
+          <textarea id="message" bind:value={message2} name="message" placeholder="Message" spellcheck="true"></textarea>
+        </div>
+        <button id="submitButton" class="submit" class:disabled={!isFormFilled}>Submit</button>
+      </form> 
+    </div>
+  </div>
+{/if}
+
+<style>
+  input {
+    border-radius: 6px;
+  }
+
+  #message {
+    width: 99.65%;
+    height: 149px;
+    background-color: #CDD7D6;
+    overflow-wrap: break-word;
+    border-radius: 8px;
+    resize: none;
+    border-color: #102542;
+    border-width: 1px;
+    margin-top: .5rem;
+    outline: none;
+    box-sizing: border-box;
+    box-shadow: 0 0 0px solid #102542;
+    transition: box-shadow 0.1s ease;
+    font-family: system-ui, sans-serif;
+  }
+  #message:focus {
+    box-shadow: 0 0 4px #102542;
+  }
+  #close {
+    background-color: #CDD7D6;
+    border-color: transparent;
+    transition: 0.25s;
+    cursor: pointer;
+    border-radius: 10px;
+    width: 30px;
+    height: 30px;
+    padding: 4px;
+    justify-content: center;
+    align-items: center;
+    appearance: fixed;
+  }
+  #close img {
+    height: 18px;
+    width: 18px;
+  }
+  #close:hover{
+    background-color: rgb(140, 167, 162);
+    transition: 0.25s;
+  }
+  .modal {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    pointer-events: none;
+    overflow: auto;
+  }
+  .modal-content {
+    width: 400px;
+    height: 270px;
+    border-radius: 15px;
+    padding: 16px;
+    background: #CDD7D6;
+    pointer-events: auto;
+    animation-name: animatemodal;
+    animation-duration: 0.5s
+    
+  }
+  .modal-header {
+    display: flex;
+    justify-content: space-between;
+  }
+  .modal-header h2 {
+    margin-top: 8px;
+    margin-left: 8px;
+    font-size: 24px;
+    color: #102542;
+  }
+  #inputs {
+    width: 49.25%;
+    height: 25px;
+    justify-self: center;
+    background-color: #CDD7D6;
+    border: 1px solid #102542;
+    outline: none;
+    box-sizing: border-box;
+    
+    box-shadow: 0 0 0px #102542;
+    transition: box-shadow 0.1s ease;
+  }
+  #inputs:focus {
+    box-shadow: 0 0 4px #102542;
+  }
+  #inputs2 {
+    width: 49.25%;
+    height: 25px;
+    justify-self: center;
+    background-color: #CDD7D6;
+    border: 1px solid #102542;
+    outline: none;
+    box-sizing: border-box;
+    
+    box-shadow: 0 0 0px #102542;
+    transition: box-shadow 0.1s ease;
+  }
+  #inputs2:focus {
+    box-shadow: 0 0 4px #102542;
+  }
+  @keyframes animatemodal {
+    from {opacity: 0}
+    to {opacity: 1}
+  }
+  .submit {
+        background-color: rgb(238, 198, 108);
+        border: none;
+        padding: 12px 20px;
+        width: 99.65%;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        margin-top: 4px;
+        cursor: pointer;
+        border-radius: 1000px;
+        font-size: 15px;
+        color: #102542;
+        font-weight: bold;
+        transition: .25s;
+  }
+  .submit:hover {
+        background-color: rgb(202, 167, 86);
+        transition: 0.25s;
+  }
+  .submit.disabled {
+      background-color: grey;
+      color: #666;
+      cursor: default;
+  }
+
+</style>
